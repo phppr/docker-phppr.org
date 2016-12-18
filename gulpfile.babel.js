bdb6f1fec8d4a6489697c6d7b2ee910e12c60a59
Fernando Moreira <nandomoreira.me@gmail.com>
2016-12-18 11:34:46 -0200

    move src, add gulp scripts task import bootstrap and font-awesome sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,7 +8,7 @@ const $ = Plugins();
 const isProduction = $.util.env.type === 'production' ? true : false;
 const browsers = ['last 2 versions'];
 const config = {
-  sass: './assets/sass',
+  src: './assets/src',
   css: './assets/css',
   js: './assets/js',
   plumberErrorHandler: {
@@ -20,26 +20,46 @@ const config = {
 };
 
 Gulp.task('stylesheets', () => {
-  return Gulp.src([`${config.sass}/*.sass`])
+  return Gulp.src([`${config.src}/sass/*.sass`])
     .pipe($.plumber(config.plumberErrorHandler))
     .pipe($.sass({
       sourceComments: isProduction ? false : 'normal',
       includePaths: [
-        config.sass,
-        Bourbon.includePaths
+        Bourbon.includePaths,
+        'assets/src/components',
+        'assets/src/sass'
       ]
     }))
     .pipe($.autoprefixer({ browsers }))
     .pipe($.combineMq())
-    .pipe(isProduction ? $.cssnano() : $.jsbeautifier({indentSize: 2}))
+    .pipe(isProduction ? $.cssnano() : $.jsbeautifier({ indentSize: 2 }))
     .pipe(isProduction ? $.rename({ suffix: '.min' }) : $.util.noop())
-    .pipe($.size({ title: 'Build and Minify Stylesheets', gzip: false, showFiles: true }))
+    .pipe($.size({ title: 'Build Stylesheets', gzip: false, showFiles: true }))
     .pipe(Gulp.dest(config.css))
     .pipe($.plumber.stop());
-})
+});
 
-.task('watch', ['stylesheets'], () => {
-  Gulp.watch(`${config.sass}/**/*.{sass,scss}`, ['stylesheets']);
-})
+Gulp.task('scripts', () => {
+  return Gulp.src([`${config.src}/javascripts/*.js`])
+    .pipe($.plumber(config.plumberErrorHandler))
+    .pipe($.include({
+      extensions: 'js',
+      includePaths: [
+        'node_modules',
+        'assets/src/components',
+        'assets/src/javascripts'
+      ]
+    }))
+    .pipe(isProduction ? $.uglify() : $.util.noop())
+    .pipe(isProduction ? $.rename({ suffix: '.min' }) : $.util.noop())
+    .pipe($.size({ title: 'Build javascripts', gzip: false, showFiles: true }))
+    .pipe(Gulp.dest(config.js))
+    .pipe($.plumber.stop());
+});
 
-.task('default', [ 'watch' ]);
+Gulp.task('watch', ['stylesheets', 'scripts'], () => {
+  Gulp.watch(`${config.src}/sass/**/*`, ['stylesheets']);
+  Gulp.watch(`${config.src}/javascripts/**/*`, ['scripts']);
+});
+
+Gulp.task('default', [ 'watch' ]);
