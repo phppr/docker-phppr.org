71142cf6356143aafbabf44f510907768911087c
Fernando Moreira <nandomoreira.me@gmail.com>
2017-02-11 19:46:52 -0200

    Remove wp_head emoji

--- src/themes/phppr/inc/optimize.php
+++ src/themes/phppr/inc/optimize.php
@@ -115,3 +115,10 @@ function disable_emojis_tinymce( $plugins ) {
 }
 
 add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
