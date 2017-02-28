68d9a57f66196d68ad2127e4ebb6ddc53282c8b4
Fernando Moreira <nandomoreira.me@gmail.com>
2017-02-28 15:41:04 -0300

    Add 404 page Not Found

--- src/themes/phppr/404.php
+++ src/themes/phppr/404.php
@@ -8,18 +8,25 @@
 
 get_header(); ?>
     <div class="container">
-	    <main id="content" class="<?php echo odin_classes_page_full(); ?>" role="main">
+	    <main id="content" class="<?php echo odin_classes_page_full(); ?> card" role="main">
+            <div class="jumbotron text-center">
+                <header class="page-header">
+                    <h1 class="page-title">Oops.. <?php _e( 'Not Found', 'odin' ); ?>!</h1>
+                </header>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'odin' ); ?></h1>
-			</header>
+                <div class="page-content">
+                    <p class="lead"><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'odin' ); ?></p>
+                    <div class="sp sp--large"></div>
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                            <?php get_search_form(); ?>
+                        </div>
+                    </div>
+                </div><!-- .page-content -->
 
-			<div class="page-content">
-				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'odin' ); ?></p>
-				<?php get_search_form(); ?>
-			</div><!-- .page-content -->
-
+                <div class="clearfix"></div>
+                <div class="sp sp--large"></div>
+            </div>
 	    </main><!-- #main -->
-        <?php get_sidebar(); ?>
     </div>
 <?php get_footer();
--- src/themes/phppr/searchform.php
+++ src/themes/phppr/searchform.php
@@ -7,13 +7,11 @@
  */
 ?>
 
-<form method="get" id="searchform" class="form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<div class="input-group">
-		<input type="search" class="form-control" name="s" id="s" value="<?php echo get_search_query(); ?>" placeholder="<?php esc_attr_e( 'Search', 'odin' ); ?>" />
-		<span class="input-group-btn">
-			<button type="submit" class="btn btn-default" value="<?php esc_attr_e( 'Search', 'odin' ); ?>">
-				<i class="glyphicon glyphicon-search"></i>
-			</button>
-		</span><!-- /input-group-btn -->
-    </div><!-- /input-group -->
-</form><!-- /searchform -->
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+    <div class="input-group input-group-lg">
+        <input type="search" class="form-control" name="s" value="<?php echo get_search_query(); ?>" placeholder="Procurar na phppr.org" required>
+        <span class="input-group-btn">
+            <button type="submit" class="btn btn-default"><i class="icon icon-search"></i></button>
+        </span>
+    </div>
+</form>
