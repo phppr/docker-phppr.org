09f92e31fdbecbb648d80b8604e9a6a3f1e353b8
Fernando Moreira <nandomoreira.me@gmail.com>
2017-04-09 15:45:19 -0300

    Removed phppr theme

--- src/themes/phppr/inc/twitter-iframe.php
+++
@@ -1,5 +0,0 @@
-<h3 class="section__title section__title--cast"><a href="https://twitter.com/search?q=%23phppr&ref_src=twsrc%5Etfw" target="_blank">No twitter</a></h3>
-<div class="card card--twitter">
-    <a class="twitter-timeline"  href="https://twitter.com/hashtag/phppr" data-widget-id="832970539924877313">phppr Tweets</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-</div>
